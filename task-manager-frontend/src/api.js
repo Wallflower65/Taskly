a926d0e5b2a546522e3196f93d0252cb9d0efb2c
Wallflower65 <phaphamanizoneleni@gmail.com>
2026-06-19 22:41:11 +0200

    fix: append /api prefix to production baseURL

--- task-manager-frontend/src/api.js
+++ task-manager-frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'https://taskly-api-502k.onrender.com'
+  baseURL: 'https://taskly-api-502k.onrender.com/api'
 });
 
 API.interceptors.request.use((config) => {
